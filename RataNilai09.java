e1aa0d50e25184ac2c117c31c5c7254babda2fb4
dinaameil <dinaameil753@gmail.com>
2024-10-25 08:30:09 +0700

    memasukkan file jobsheet9

---
+++ RataNilai09.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class RataNilai09 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int i,j;
+        float nilaiMhs,totalNilai,rataNilai;
+        
+        i=1;
+
+
+        for (i=1;i<=5;i++){
+            totalNilai=0;
+            System.out.println("Input Nilai Mahasiswa ke "+i);
+            for (j=1;j<=5;j++){
+                
+                System.out.print("Nilai ke-"+j+"= ");
+                nilaiMhs=sc.nextInt();
+                totalNilai+=nilaiMhs;
+            }
+            rataNilai=totalNilai/5;
+            System.out.println("Rata-rata Nilai Mahasiswa ke "+i+ " adalah "+ rataNilai);
+            
+        }
+        
+    }
+}
